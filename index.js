6848bc578570bd14cf08a3e8912b50f51bbe9108
skaipio <senni.kaipio@gmail.com>
2017-02-18 23:57:38 +0200

    Some refactoring

--- index.js
+++ index.js
@@ -1,103 +1,92 @@
-const defaultSides = 3;
+function App() {
+  const defaultSides = 3;
+  const self = this;
+  this.sides = defaultSides;
 
-const bindRefresh = (app) => {
-  $('#sides-form').submit((event) => {
-    event.preventDefault();
-    var sidesInput = $('#sides');
-    var sides = parseInt(sidesInput.val());
-    if (!Number.isInteger(sides) || sides < defaultSides) {
-      sides = defaultSides;
+  function main( tFrame ) {
+    self.stopMain = window.requestAnimationFrame( main );
+    var nextTick = self.lastTick + self.tickLength;
+    var numTicks = 0;
+
+    //If tFrame < nextTick then 0 ticks need to be updated (0 is default for numTicks).
+    //If tFrame = nextTick then 1 tick needs to be updated (and so forth).
+    //Should keep track of how large numTicks is.
+    //If it is large, then either game was asleep, or the machine cannot keep up.
+    if (tFrame > nextTick) {
+      var timeSinceTick = tFrame - self.lastTick;
+      numTicks = Math.floor( timeSinceTick / self.tickLength );
     }
-    polygonToBuffer(app, sides);
-    drawPolygon(app, sides);
-  });
-}
 
-const webGLStart = () => {
-  PhiloGL('polygon-canvas', {
-    program: {
-      from: 'ids',
-      vs: 'shader-vs',
-      fs: 'shader-fs'
-    },
-    onError: function() {
-      alert("An error ocurred while loading the application");
-    },
-    onLoad: function(app) {
-      bindRefresh(app);
-
-      var gl = app.gl,
-          canvas = app.canvas,
-          program = app.program,
-          camera = app.camera;
-
-      gl.viewport(0, 0, canvas.width, canvas.height);
-      gl.clearColor(0, 0, 0, 1);
-      gl.clearDepth(1);
-      gl.enable(gl.DEPTH_TEST);
-      gl.depthFunc(gl.LEQUAL);
-
-      polygonToBuffer(app, defaultSides);
-      drawPolygon(app, defaultSides);
-    }
-  });
-}
-
-// Offset to rotate 90 degrees
-const angleOffset = Math.PI / 2;
-
-const polygonToBuffer = (app, sides) => {
-  const halved = Math.floor(sides / 2);
-  var fullAngle = 2 * Math.PI;
-  var angle = fullAngle / sides;
-  var vertexPositions = [];
-  for (var side = 0; side < halved; ++side) {
-    var finalAngle = angle * side + angleOffset;
-    var finalAngleOpposite = angle * (sides - side - 1) + angleOffset;
-    var x1 = Math.cos(finalAngle), x2 = Math.cos(finalAngleOpposite);
-    var y1 = Math.sin(finalAngle), y2 = Math.sin(finalAngleOpposite);
-    vertexPositions.push([x1, y1, 0]);
-    vertexPositions.push([x2, y2, 0]);
-    console.log('vertex', x1, y1);
-    console.log('opposite', x2, y2);
+    queueUpdates( numTicks );
+    render( tFrame );
+    self.lastRender = tFrame;
   }
 
-  // If odd number of sides, add the last vertex
-  if (sides % 2 > 0) {
-    var x = Math.cos(angle * halved + angleOffset);
-    var y = Math.sin(angle * halved + angleOffset);
-    vertexPositions.push([x, y, 0]);
+  const bindSidesInput = () => {
+    self.sidesInput.change(() => {
+      self.sides = parseInt(self.sidesInput.val());
+      if (!Number.isInteger(self.sides)) {
+        self.sides = defaultSides;
+      } else if (self.sides < defaultSides) {
+        self.sidesInput.val(defaultSides);
+        self.sides = defaultSides;
+      }
+    });
   }
 
-  //vertexPositions = translate(vertexPositions, [3, 0, 0]);
-  app.program.setBuffers({
-    'triangle': {
-      attribute: 'aVertexPosition',
-      value: new Float32Array(flatten(vertexPositions)),
-      size: 3
+  this.webGLStart = () => {
+    PhiloGL('polygon-canvas', {
+      program: {
+        from: 'ids',
+        vs: 'shader-vs',
+        fs: 'shader-fs'
+      },
+      onError: function() {
+        alert("An error ocurred while loading the application");
+      },
+      onLoad: function(app) {
+        self.app = app;
+        self.sidesInput = $('#sides');
+        self.sides = 3;
+
+        bindSidesInput();
+
+        var gl = app.gl,
+            canvas = app.canvas,
+            program = app.program,
+            camera = app.cameravar;
+
+
+        gl.viewport(0, 0, canvas.width, canvas.height);
+        gl.clearColor(0, 0, 0, 1);
+        gl.clearDepth(1);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+
+        self.lastTick = performance.now();
+        self.lastRender = self.lastTick; //Pretend the first draw was on first update.
+        self.tickLength = 50; //This sets your simulation to run at 20Hz (50ms)
+
+        //setInitialState();
+        main(performance.now()); // Start the cycle*/
+      }
+    });
+  }
+
+  function render(tFrame) {
+    polygonToBuffer(self.app, self.sides, self.lastRender);
+    drawPolygon(self.app, self.sides);
+  }
+
+  function queueUpdates( numTicks ) {
+    for(var i=0; i < numTicks; i++) {
+      self.lastTick = self.lastTick + self.tickLength; //Now lastTick is this tick.
+      //update( self.lastTick );
     }
-  });
+  }
 }
 
-const drawPolygon = (app, sides) => {
-  var gl = app.gl,
-      program = app.program,
-      camera = app.camera;
-
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  camera.view.id();
-  //Draw Triangle
-  camera.view.$translate(-1.5, 0, -7);
-  program.setUniform('uMVMatrix', camera.view);
-  program.setUniform('uPMatrix', camera.projection);
-  program.setBuffer('triangle');
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, sides);
+function startApp() {
+  var app = new App();
+  app.webGLStart();
 }
-
-const translate = (vertices, translationVector) => {
-  return vertices.map((vertex) =>
-    vertex.map((component, i) =>
-      component + translationVector[i]));
-};
-
-const flatten = (arr) => arr.reduce((a, b) => a.concat(b));
---
+++ rendering.js
@@ -0,0 +1,59 @@
+const polygonToBuffer = (app, sides, time) => {
+  // Offset to rotate 90 degrees
+  const angleOffset = Math.PI / 2 + time * 0.001;
+  const halved = Math.floor(sides / 2);
+  var fullAngle = 2 * Math.PI;
+  var angle = fullAngle / sides;
+  var vertexPositions = [];
+  for (var side = 0; side < halved; ++side) {
+    var finalAngle = angle * side + angleOffset;
+    var finalAngleOpposite = angle * (sides - side - 1) + angleOffset;
+    var x1 = Math.cos(finalAngle), x2 = Math.cos(finalAngleOpposite);
+    var y1 = Math.sin(finalAngle), y2 = Math.sin(finalAngleOpposite);
+    vertexPositions.push([x1, y1, 0]);
+    vertexPositions.push([x2, y2, 0]);
+    //console.log('vertex', x1, y1);
+    //console.log('opposite', x2, y2);
+  }
+
+  // If odd number of sides, add the last vertex
+  if (sides % 2 > 0) {
+    var x = Math.cos(angle * halved + angleOffset);
+    var y = Math.sin(angle * halved + angleOffset);
+    vertexPositions.push([x, y, 0]);
+  }
+
+  // vertexPositions = translate(vertexPositions, [0.0001*time, 0, 0]);
+
+  //vertexPositions = translate(vertexPositions, [3, 0, 0]);
+  app.program.setBuffers({
+    'triangle': {
+      attribute: 'aVertexPosition',
+      value: new Float32Array(flatten(vertexPositions)),
+      size: 3
+    }
+  });
+}
+
+const drawPolygon = (app, sides) => {
+  var gl = app.gl,
+      program = app.program,
+      camera = app.camera;
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  camera.view.id();
+  //Draw Triangle
+  camera.view.$translate(0, 0, -5);
+  program.setUniform('uMVMatrix', camera.view);
+  program.setUniform('uPMatrix', camera.projection);
+  program.setBuffer('triangle');
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, sides);
+}
+
+const translate = (vertices, translationVector) => {
+  return vertices.map((vertex) =>
+    vertex.map((component, i) =>
+      component + translationVector[i]));
+};
+
+const flatten = (arr) => arr.reduce((a, b) => a.concat(b));
